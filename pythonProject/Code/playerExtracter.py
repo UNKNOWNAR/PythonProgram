366a518d30c3a38d6c7922f66e14fdd231ecb50c
UnknownAR <amiarinjaysarkar@gmail.com>
2024-11-28 22:39:51 +0530

    Added team name, year from file_path to dictionary; generated unique codes for teams.

--- pythonProject/Code/playerExtracter.py
+++ pythonProject/Code/playerExtracter.py
@@ -2,6 +2,7 @@ import pandas as pd
 class playerExtractor:
     def __init__(self):
         self.players = {}# playerType,YearsPlayed,Captain,WicketKeeper,Batting Style,Bowling Style
+        idx = 0
 
     def skip_line(self, f, num_lines=1):
         """Skip a specified number of lines in the file."""
@@ -22,21 +23,23 @@ class playerExtractor:
             self.skip_line(f)  # Skip the next line after identifying the type
             return "BOWLERS"
 
-    def read_file(self, file_path,year):
+    def read_file(self, file_path):
         with open(file_path, "r") as f:
+            player_team = self.extract_team(file_path)
+            year = self.extract_year(file_path)
             player_name = ""
             player_type = self.player_type(f.readline().strip(),f)
             for line in f:
                 line = line.strip()
                 if line == "Wicketkeeper Batter":
-                    self.players[player_name][3] = True
+                    self.players[player_name][2] = True
                 elif line == "Age:":
                     self.skip_line(f)
                 elif line == "Batting:":
-                    self.players[player_name][4] = next(f).strip()
+                    self.players[player_name][3] = next(f).strip()
                     line = next(f, None)
                     if line and line.strip() =="Bowling:":
-                        self.players[player_name][5] = next(f).strip()
+                        self.players[player_name][4] = next(f).strip()
                         line = next(f, None)
                         if line:
                             line = line.strip()
@@ -48,7 +51,7 @@ class playerExtractor:
                 elif line:  # Only add if the line is not empty
                     player_name = line
                     if player_name not in self.players:
-                        self.players[player_name] = [player_type,None,False,False,None,None]
+                        self.players[player_name] = [player_type,None,False,None,None,player_team]
                     if self.players[player_name][1] is None:
                         self.players[player_name][1] = str(year)
                     else:
@@ -67,31 +70,49 @@ class playerExtractor:
                                 self.skip_line(f)
                                 self.skip_line(f)
                         elif line and line.startswith("("):
-                            self.players[player_name][2]= True
                             line = next(f).strip()
                             if line == "Wicketkeeper Batter":
-                                self.players[player_name][3] = "Wicketkeeper"
+                                self.players[player_name][2] = True
                         elif line and line.strip() == "Age:":
                             self.skip_line(f)
                     except StopIteration:
                         pass
 
     def create_file(self):  # creating a csv file
-        # playerType, YearsPlayed, Captain, WicketKeeper, Batting Style, Bowling Style
+        # playerType, YearsPlayed, WicketKeeper, Batting Style, Bowling Style, Team
         player_df = pd.DataFrame.from_dict(
             self.players,
             orient='index',
-            columns=['PlayerType', 'YearsPlayed', 'Captain', 'WicketKeeper', 'BattingStyle', 'BowlingStyle']
+            columns=['PlayerType', 'YearsPlayed', 'WicketKeeper', 'BattingStyle', 'BowlingStyle','Team']
         )
         player_df.to_csv("E:\\Cricket TicTacToe Game\\RCB_Players.csv",)
         print(player_df)
 
+    def extract_team(self,file_path):
+        # Start from the end, skipping the file extension
+        idx = len(file_path) - 5
+        team = ""
+        while  file_path[idx] != "\\":
+            team = file_path[idx] + team
+            idx -= 1
+        self.idx = idx
+        return team
+
+    def extract_year(self,file_path):
+        idx = self.idx-2
+        year = ""
+        #print(file_path[idx])
+        while file_path[idx] != "\\":
+            year = file_path[idx] + year
+            idx -= 1
+        return year
+
 if __name__ == "__main__":
-    file_path = [r"E:\\Cricket TicTacToe Game\\2008\\RCB_2008.txt",
-                 r"E:\\Cricket TicTacToe Game\\2009\\RCB_2009.txt",
+    file_path = [r"E:\\Cricket TicTacToe Game\\2008\\RCB.txt",
+                 r"E:\\Cricket TicTacToe Game\\2009\\RCB.txt",
                  r"E:\\Cricket TicTacToe Game\\2010\\RCB.txt"]
     team = playerExtractor()
     for idx,file in enumerate(file_path):
-        team.read_file(file,2008+idx)
+        team.read_file(file)
     team.create_file()
     print(len(team.players))
